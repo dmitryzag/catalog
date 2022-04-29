622e819f530552e754a88594b28fa8b8d67da07f
dmitryzag <zagibalovdmitry@gmail.com>
2022-04-29 14:37:16 +0700

    v4

--- base/views.py
+++ base/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render
 from .models import Item, Category
-
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 
 def catalog(req, args):
-    slugs = filter(lambda elm: elm != '', args.split('/'))
-    items = Item.objects.all()
+    slugs = list(filter(lambda elm: elm != '', args.split('/')))
+    items = Item.objects.filter(category__slug=slugs[-1])
     categories = Category.objects.filter(parent__isnull=True)
     breadcrumbs = []
     url = '/'
+    # print(item[0].category_id)
 
     for slug in slugs:
         url = '{0}{1}/'.format(url, slug)
         name = Category.objects.get(slug=slug)
         breadcrumb = {'slug': slug, 'url': url, 'name': name}
         breadcrumbs.append(breadcrumb)
-
     context = {'categories': categories, 'breadcrumbs': breadcrumbs, 'items': items}
     return render(req, 'main.html', context)
