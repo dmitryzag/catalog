65287ab5974ebdbeb2660da31a4a702917ed0840
dmitryzag <zagibalovdmitry@gmail.com>
2022-05-05 16:30:25 +0700

    pag fix

--- base/views.py
+++ base/views.py
@@ -6,10 +6,16 @@ from django.core.paginator import Paginator
 def catalog(req, args):
     slugs = list(filter(lambda elm: elm != '', args.split('/')))
     items = [] if not slugs else Item.objects.filter(category__slug=slugs[-1])
+    # items = [] if not slugs else Item.objects.filter(category__slug=slugs[-1])
+    # if not items and Item.objects.filter(category__parent__slug=slugs[-1]):
+    #     items = [] if not slugs else Item.objects.filter(category__parent__slug=slugs[-1])
+    # elif not Item.objects.filter(category__parent__slug=slugs[-1]) \
+    #         and Item.objects.filter(category__parent__parent__slug=slugs[-1]):
+    #     items = [] if not slugs else Item.objects.filter(category__parent__parent__slug=slugs[-1])
+
     paginator = Paginator(items, 12)
     page_number = req.GET.get('page')
     page_obj = paginator.get_page(page_number)
-
 
     categories = Category.objects.filter(parent__isnull=True)
     breadcrumbs = []
@@ -21,11 +27,11 @@ def catalog(req, args):
         breadcrumb = {'slug': slug, 'url': url, 'name': name}
         breadcrumbs.append(breadcrumb)
 
-    # attempt = Category.objects.filter(child__parent__slug=slugs[-1])
-    # for a in attempt:
-    #     print(a.name)
-    # attempt2 = attempt.filter()
+    # attempt = Item.objects.filter(category__parent__parent__slug=slugs[-1])
+    # attempt1 = Item.objects.filter(category__parent__slug=slugs[-1])
+    #
     # print(attempt)
+    # print(attempt1)
     context = {'categories': categories, 'breadcrumbs': breadcrumbs, 'items': items, 'page_obj': page_obj}
     return render(req, 'main.html', context)
 
@@ -36,15 +42,6 @@ def search(req):
     paginator = Paginator(items, 12)
     page_number = req.GET.get('page', 1)
     page_obj = paginator.page(page_number)
-    query = req.GET.get('search', '')
-    r = req.GET.dict()
-
-    for key, value in r.items():
-        print(key, value)
-
-    # print("ЭТО КВЕРИ", req.GET)
-
-
     categories = Category.objects.filter(parent__isnull=True)
-    context = {'page_obj': page_obj, 'categories': categories, 'page_number': page_number,'query': query}
+    context = {'page_obj': page_obj, 'categories': categories, 'page_number': page_number}
     return render(req, 'main.html', context)
