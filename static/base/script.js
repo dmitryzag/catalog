cea2356959f05ad7267e197920f6fa95cffe176c
dmitryzag <zagibalovdmitry@gmail.com>
2022-05-26 13:27:40 +0700

    catalog

--- static/base/script.js
+++ static/base/script.js
@@ -1,6 +1,4 @@
-let $crsf_token = $('[name="csrfmiddlewaretoken"]').attr('value');
-
-
+let $csrf_token = $('[name="csrfmiddlewaretoken"]').attr('value');
 $(document).ready(function(){
     $('.catalog_link').click(function(event){
         event.preventDefault();
@@ -8,10 +6,9 @@ $(document).ready(function(){
             type: 'POST',
             url: $(this).attr('href'),
             dataType: 'json',
-            headers: {'X-CSRFToken': $crsf_token},
+            headers: {'X-CSRFToken': $csrf_token},
             success: function (data){
-                $('#catalog').html(data.test)
-                console.log(data)
+                console.log($('#catalog'))
             }
         })
     })
