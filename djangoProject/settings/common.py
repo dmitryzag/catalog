a5182328040c5d2b13cc4f21ebb6b1e038b800cd
dmitryzag <zagibalovdmitry@gmail.com>
2022-05-27 19:37:27 +0700

    catalog

--- djangoProject/settings/common.py
+++ djangoProject/settings/common.py
@@ -124,5 +124,5 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '../../media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/home/zagibalovda/PycharmProjects/djangoProject/media')
 MEDIA_URL = 'media/'
