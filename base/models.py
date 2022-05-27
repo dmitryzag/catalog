539775aa01a73960a020741895cb0727609855d7
dmitryzag <zagibalovdmitry@gmail.com>
2022-05-27 19:07:58 +0700

    catalog

--- base/models.py
+++ base/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Item(models.Model):
     desc = models.CharField(max_length=200)
     image = models.ImageField(upload_to='items/', null=True, blank=True)
-    slug = models.SlugField()
     category = models.ForeignKey('Category', related_name='items', on_delete=models.CASCADE, null=True, blank=True)
 
     def get_absolute_url(self):
