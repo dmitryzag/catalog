21cab4fdea85aedf1cb218a0e21eb02e0434a490
dmitryzag <zagibalovdmitry@gmail.com>
2022-05-27 19:59:20 +0700

    catalog

--- base/views.py
+++ base/views.py
@@ -4,12 +4,16 @@ from django.core.paginator import Paginator
 from .utils import get_page, tree, create_bread, get_items
 from django.http import JsonResponse
 from django.template.loader import render_to_string
+from django.http import Http404
 
 
 def catalog(req, args):
     slugs = list(filter(lambda elm: elm != '', args.split('/')))
     categories = tree()
-    items = get_items(categories, ''.join(slugs[-1:]))
+    try:
+        items = get_items(categories, ''.join(slugs[-1:]))
+    except TypeError:
+        raise Http404
     paginator = Paginator(items, 12)
     page = get_page(paginator, req.GET)
     breadcrumbs = create_bread(categories, slugs) if slugs else ''
