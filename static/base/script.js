bc28bb55078a97cd1012032ba0717369c2ff02a1
dmitryzag <zagibalovdmitry@gmail.com>
2022-05-27 16:40:40 +0700

    catalog

--- static/base/script.js
+++ static/base/script.js
@@ -1,11 +1,30 @@
 let $csrf_token = $('[name="csrfmiddlewaretoken"]').attr('value');
+// $(document).ready(function(){
+//     $('.catalog_link').click(function(event){
+//         event.preventDefault();
+//         let path = $(this).attr('href')
+//         $.ajax({
+//             type: 'POST',
+//             url: path,
+//             dataType: 'json',
+//             headers: {'X-CSRFToken': $csrf_token},
+//             success: function (data){
+//                 window.history.pushState(data, '', path)
+//                 $('#catalog').replaceWith(data.rendered)
+//             }
+//         })
+//         event.stopPropagation();
+//
+//     })
+// });
+
 $(document).ready(function(){
-    $('.catalog_link').click(function(event){
+    $(document).on('click', '.catalog_link', function(event){
         event.preventDefault();
         let path = $(this).attr('href')
         $.ajax({
             type: 'POST',
-            url: $(this).attr('href'),
+            url: path,
             dataType: 'json',
             headers: {'X-CSRFToken': $csrf_token},
             success: function (data){
@@ -13,9 +32,5 @@ $(document).ready(function(){
                 $('#catalog').replaceWith(data.rendered)
             }
         })
-    })
-
-    $('.breadcrumb').click(function (event){
-        event.preventDefault();
     })
 });
