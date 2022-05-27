05ccbc23a70ddbe882945a5f60d6908cab265ecd
dmitryzag <zagibalovdmitry@gmail.com>
2022-05-27 18:15:44 +0700

    catalog

--- djangoProject/settings/common.py
+++ djangoProject/settings/common.py
@@ -5,7 +5,7 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
