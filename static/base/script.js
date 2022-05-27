a336e902a57658309347f35c226fb1848c0f050a
dmitryzag <zagibalovdmitry@gmail.com>
2022-05-27 16:42:39 +0700

    catalog

--- static/base/script.js
+++ static/base/script.js
@@ -1,23 +1,4 @@
 let $csrf_token = $('[name="csrfmiddlewaretoken"]').attr('value');
-// $(document).ready(function(){
-//     $('.catalog_link').click(function(event){
-//         event.preventDefault();
-//         let path = $(this).attr('href')
-//         $.ajax({
-//             type: 'POST',
-//             url: path,
-//             dataType: 'json',
-//             headers: {'X-CSRFToken': $csrf_token},
-//             success: function (data){
-//                 window.history.pushState(data, '', path)
-//                 $('#catalog').replaceWith(data.rendered)
-//             }
-//         })
-//         event.stopPropagation();
-//
-//     })
-// });
-
 $(document).ready(function(){
     $(document).on('click', '.catalog_link', function(event){
         event.preventDefault();
